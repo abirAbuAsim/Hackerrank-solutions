b5b029c63fe78a51765489d50579e2364fddb437
absakDev <abir.sakif@gmail.com>
2016-05-30 19:45:41 +0600

    Domain - Java_Introduction - Welcome to Java, Java Stdin and Stdout1; Day8 of 30 Days

---
+++ Java/Introduction/Welcome_To_Java!/Solution.java
@@ -0,0 +1,10 @@
+/*
+	Verdict	: Accepted
+	OJ 		: Hackerrank	
+*/
+public class Solution {
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+		System.out.println("Hello Java.");
+	}
+}
